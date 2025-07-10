bc4bc3a72a1800c580095dc4199670328f7f632b
Dina73 <dina7.3.2003@gmail.com>
2025-07-10 20:07:49 +0300

    Update otto.py

--- otto.py
+++ otto.py
@@ -9,8 +9,9 @@ def main():
     cp= 1.005   # kJ/kg.K
     R= 0.2871   # kJ/kg.K
 
-     st.markdown("Input Parameters")
-      col1, col2 = st.columns(2)
+    st.markdown("Input Parameters")
+    
+    col1, col2 = st.columns(2)
     with col1:
     compression_ratio = st.number_input("Compression Ratio (r)", min_value=1.0)
     heat_added = st.number_input("Heat Added (Qin) [kJ/kg]", min_value=0.0)
